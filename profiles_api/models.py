f7ac9c46a02f3d6b9be2186da7a934b1b491e0ea
Kolby Morris <morrs.kolby00@gmail.com>
2020-03-28 23:48:21 -0500

    some commmit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -16,21 +16,22 @@ class UserProfileManager(BaseUserManager, UserAdmin):
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name,)
-        user.save(using=self._db)
+        user = User(
+            email=email, is_staff=False, is_active=True,
+            is_superuser=False,
+            last_login=now, date_joined=now, **extra_fields
+        )
+        #user = self.model(email=email, name=name,)
+        #user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
-        user =User(
-            email=email, is_staff=False, is_active=True,
-            is_superuser=False,
-            last_login=now, date_joined=now, **extra_fields
-            self.create_user(email, name, password=user.set_password(password))
-
+        user = self.create_user(email, name, password=user.set_password(password))
             user.is_superuser = True
-            user.is_staff = True)
+            user.is_staff = True
+
         user.set_password(password)
         user.save(using=self._db)
 
