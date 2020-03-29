d70e2df5e28f6301ad15445c0868eb9fcdda7dc9
Kolby Morris <morrs.kolby00@gmail.com>
2020-03-29 12:04:00 -0500

    some commmit

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,27 +1,12 @@
 from django.contrib import admin
 from profiles_api import models
 
+from __future__ import unicode_literals
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users import models
+
+admin.site.register(models.MyUser, UserAdmin)
 admin.site.register(models.UserProfile)
 admin.site.register(models.ProfileFeedItem)
-
-
-class UserAdmin(BaseUserManager, UserAdmin):
-    def create_user(self, email, name, password=None, **extra_fields):
-        # Create a new user profile
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        email = self.normalize_email(email)
-
-        user = User(
-            email=email, is_staff=False, is_active=True,
-            is_superuser=False,
-        )
-
-        #save the password here:
-        user.set_password(password)
-        user = self.model(email=email, **extra_fields)
-        user.save(using=self._db)
-
-        return user
